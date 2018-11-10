219849573221c241e5972e96ee7dd751dd4e669f
anatooliy <anatooliy@ukr.net>
2018-11-10 03:35:14 +0200

    add edits and delets methods

--- TODO-list/Controllers/HomeController.cs
+++ TODO-list/Controllers/HomeController.cs
@@ -18,7 +18,12 @@ namespace TODO_list.Controllers
 
         [HttpGet]
         public ActionResult Create()
-        {            
+        {
+            if (toDoItems.Count > 0)
+                ViewBag.NewId = toDoItems.Max(t => t.Id) + 1;
+            else
+                ViewBag.NewId = 1;
+
             return View();
         }
 
@@ -27,8 +32,54 @@ namespace TODO_list.Controllers
         {
             toDoItems.Add(toDo);
             
-            return RedirectToAction("Index", toDoItems);
+            return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            ToDo t = toDoItems.Find(x => x.Id == id);
+
+            if (t == null)
+            {
+                return HttpNotFound();
+            }
+            return View(t);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ToDo t = toDoItems.Find(x => x.Id == id);
+            if (t == null)
+            {
+                return HttpNotFound();
+            }
+
+            toDoItems.Remove(t);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return HttpNotFound();
+
+            ToDo t = toDoItems.Find(x => x.Id == id);
+            if(t == null)
+                return HttpNotFound();
+
+            return View(t);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ToDo toDo)
+        {
+            int index = toDoItems.FindIndex(x => x.Id == toDo.Id);
+            toDoItems[index] = toDo;
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- TODO-list/Models/TodoModel.cs
+++ TODO-list/Models/TodoModel.cs
@@ -4,6 +4,7 @@ namespace TODO_list.Models
 {
     public class ToDo
     {
+        public int Id { get; set; }
         public string ToDoDescription { get; set; }
     }
 }
