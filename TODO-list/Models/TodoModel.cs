e29495d72a08132ce37fba9a0700a5eeba4bc31d
anatooliy <anatooliy@ukr.net>
2018-11-10 16:39:16 +0200

    did some layout

--- TODO-list/Models/TodoModel.cs
+++ TODO-list/Models/TodoModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TODO_list.Models
 {
     public class ToDo
     {
         public int Id { get; set; }
+
+        [Display(Name = "Наименование")]
+        [Required]
         public string ToDoDescription { get; set; }
     }
 }
